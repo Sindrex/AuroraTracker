5a4631ebe0fa3389655d8820af7c17ff51beed47
sindre.paulshus <sindre.paulshus@bouvet.no>
2021-04-28 14:41:01 +0200

    Minor changes, maybe fix?

---
+++ AuroraTracker/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AuroraTracker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public HomeController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            Console.WriteLine($"HomeController: Get");
+            await Task.Delay(1);
+            return Accepted(1);
+        }
+    }
+}
